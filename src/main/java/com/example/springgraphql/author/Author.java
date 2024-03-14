91008d006f8ea84c4b28cdf3c35d14839486287c
Dyego Penha <dyegopenha@hotmail.com>
2024-03-14 18:57:36 -0300

    first commit

---
+++ src/main/java/com/example/springgraphql/author/Author.java
@@ -0,0 +1,11 @@
+package com.example.springgraphql.author;
+
+import lombok.Data;
+
+@Data
+public class Author {
+
+    private String id;
+    private String name;
+    private String thumbnail;
+}
---
+++ src/main/java/com/example/springgraphql/author/AuthorController.java
@@ -0,0 +1,23 @@
+package com.example.springgraphql.author;
+
+import com.example.springgraphql.post.Post;
+import com.example.springgraphql.post.PostDao;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class AuthorController {
+
+    private final PostDao postDao;
+
+    public AuthorController(PostDao postDao) {
+        this.postDao = postDao;
+    }
+
+    @SchemaMapping
+    public List<Post> posts(Author author) {
+        return postDao.getAuthorPosts(author.getId());
+    }
+}
